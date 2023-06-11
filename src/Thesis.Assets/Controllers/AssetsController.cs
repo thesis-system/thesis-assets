fa4648c468f2a393dbee84aac109e65502a3cdd2
Zagidin Selimov <seljmov@list.ru>
2023-06-11 23:17:15 +0400

    add docs

--- src/Thesis.Assets/Controllers/AssetsController.cs
+++ src/Thesis.Assets/Controllers/AssetsController.cs
@@ -25,8 +25,20 @@ public class AssetsController : ControllerBase
         _context = context;
     }
 
+    /// <summary>
+    /// Получить информацию об активе по идентификатору
+    /// </summary>
+    /// <param name="id">Идентификатор актива</param>
+    /// <response code="200">Информация об активе</response>
+    /// <response code="401">Токен доступа истек</response>
+    /// <response code="404">Актив не найден</response>
+    /// <response code="500">Ошибка сервера</response>
     [HttpGet("{id:guid}")]
-    public async Task<IActionResult> GetAssetInfo(Guid id)
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AssetDto))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAssetInfo([FromRoute] Guid id)
     {
         var asset = await _context.Assets
             .Include(asset => asset.Category)
@@ -52,24 +64,38 @@ public class AssetsController : ControllerBase
         return Ok(assetDto);
     }
     
+    /// <summary>
+    /// Получить информацию об структуре по идентификаторам квартир
+    /// </summary>
+    /// <param name="apartmentIds">Идентификаторы квартир</param>
+    /// <response code="200">Структура собственности</response>
+    /// <response code="400">Не указаны идентификаторы квартир</response>
+    /// <response code="401">Токен доступа истек</response>
+    /// <response code="404">Квартиры не найдены</response>
+    /// <response code="500">Ошибка сервера</response>
     [HttpGet]
-    public async Task<IActionResult> GetStructureByApartments([FromQuery] ICollection<Guid> apartments)
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Node>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetStructureByApartments([FromQuery] ICollection<Guid> apartmentIds)
     {
-        if (!apartments.Any()) return BadRequest("Не указаны идентификаторы квартир");
-        apartments = apartments.Distinct().ToList();
+        if (!apartmentIds.Any()) return BadRequest("Не указаны идентификаторы квартир");
+        apartmentIds = apartmentIds.Distinct().ToList();
         
-        var notFoundApartments = apartments
+        var notFoundApartments = apartmentIds
             .Where(apartment => !_context.Assets.Any(asset => asset.Id == apartment && asset.Category.Name == "Квартира"))
             .ToList();
 
         if (notFoundApartments.Any())
-            return BadRequest($"Не найдены квартиры с идентификаторами: {string.Join(", ", notFoundApartments)}"); 
+            return NotFound($"Не найдены квартиры с идентификаторами: {string.Join(", ", notFoundApartments)}"); 
         
         var assetById = await _context.Assets
             .ToDictionaryAsync(asset => asset.Id, asset => asset);
 
         var apartmentsParentsIdsById = await _context.Assets
-            .Where(asset => apartments.Contains(asset.Id))
+            .Where(asset => apartmentIds.Contains(asset.Id))
             .ToDictionaryAsync(asset => asset.Id, asset => asset.Parents);
 
         var complexes = BuildTrees(assetById, apartmentsParentsIdsById).ToList();
